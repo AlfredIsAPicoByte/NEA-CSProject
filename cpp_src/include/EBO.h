77e0ffde2ed57d2c7cf0239313b346091101d0cf
knownkreatives <181346448+AlfredIsAPicoByte@users.noreply.github.com>
2025-08-25 14:37:41 +0100

    Added Vertex structure to objects

--- cpp_src/include/EBO.h
+++ cpp_src/include/EBO.h
@@ -1,21 +1,22 @@
 #ifndef EBO_CLASS_H
 #define EBO_CLASS_H
 
-#include<glad/glad.h>
+#include <glad/glad.h>
+#include <vector>
 
 class EBO
 {
 public:
-	// Reference ID of the Vertex Buffer Object
+	// ID reference of Elements Buffer Object
 	GLuint ID;
-	// Constructor that generates a Vertex Buffer Object and links it to vertices
-	VBO(GLfloat* vertices, GLsizeiptr size);
+	// Constructor that generates a Elements Buffer Object and links it to indices
+	EBO(std::vector<GLuint>& indices);
 
-	// Binds the VBO
+	// Binds the EBO
 	void Bind();
-	// Unbinds the VBO
+	// Unbinds the EBO
 	void Unbind();
-	// Deletes the VBO
+	// Deletes the EBO
 	void Delete();
 };
 
