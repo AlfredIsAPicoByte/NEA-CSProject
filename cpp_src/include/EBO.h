43c32d9754f898ae597730dd898fab695bde22c4
knownkreatives <181346448+AlfredIsAPicoByte@users.noreply.github.com>
2025-08-25 14:34:13 +0100

    Fixed EBO

--- cpp_src/include/EBO.h
+++ cpp_src/include/EBO.h
@@ -1,9 +1,9 @@
-#ifndef VBO_CLASS_H
-#define VBO_CLASS_H
+#ifndef EBO_CLASS_H
+#define EBO_CLASS_H
 
 #include<glad/glad.h>
 
-class VBO
+class EBO
 {
 public:
 	// Reference ID of the Vertex Buffer Object
