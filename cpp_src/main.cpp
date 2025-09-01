4d832fd76736f77075294624f05a6ea92b75238e
knownkreatives <65620817+knownkreatives@users.noreply.github.com>
2025-09-01 19:35:24 +0100

    Update main.cpp

--- cpp_src/main.cpp
+++ cpp_src/main.cpp
@@ -2,82 +2,28 @@
 #include<glad/glad.h>
 #include<GLFW/glfw3.h>
 
-#include "Debug.h"
-#include "Mesh.h"
+#include "Engine.h"
 
-void processInput(GLFWwindow *window)
-{
-    if(glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
-}
-
-int main()
-{
+int main(){
 	// Initialize GLFW
-	glfwInit();
-
-	// Tell GLFW what version of OpenGL we are using 
-	// In this case we are using OpenGL 3.3
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
-	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
-	// Tell GLFW we are using the CORE profile
-	// So that means we only have the modern functions
-	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-	// enable error logging
-	glfwWindowHint(GLFW_OPENGL_DEBUG_CONTEXT, true);  
+	InitGLFW();
 
 	// Create a GLFWwindow object of 800 by 800 pixels, naming it "YoutubeOpenGL"
-	GLFWwindow* window = glfwCreateWindow(800, 800, "YoutubeOpenGL", NULL, NULL);
-	// Error check if the window fails to create
-	if (window == NULL)
-	{
-		std::cout << "Failed to create GLFW window" << std::endl;
-		glfwTerminate();
-		return -1;
-	}
-	// Introduce the window into the current context
-	glfwMakeContextCurrent(window);
+	GLFWwindow* window = createWindow(800, 800, "YoutubeOpenGL");
+	if (window == nullptr) return -1;
 
-	//Load GLAD so it configures OpenGL
-	gladLoadGL();
+	// Load GLAD so it configures OpenGL
+	InitGLAD();
 
-	// Specify the viewport of OpenGL in the Window
-	// In this case the viewport goes from x = 0, y = 0, to x = 800, y = 800
-	glViewport(0, 0, 800, 800);
+	// Setup OpenGL debug context
+	SetupOpenGLDebug();
 
-	if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
-	{
-		std::cout << "OpenGL debug callback" << std::endl;
+	// Main render loop
+	MainLoop(window, [](GLFWwindow* window){
+	}, Color("#7999bdff"));
 
-		glEnable(GL_DEBUG_OUTPUT);
-		glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS); 
-		glDebugMessageCallback(glDebugOutput, nullptr);glDebugMessageControl(GL_DEBUG_SOURCE_API, 
-                      GL_DEBUG_TYPE_ERROR, 
-                      GL_DEBUG_SEVERITY_HIGH,
-                      0, nullptr, GL_TRUE); 
-	} else {
-		std::cout << "No debug context available" << std::endl;
-	}
+	// Clean up and exit
+	CleanUp(window);
 
-	// Main while loop
-	while (!glfwWindowShouldClose(window))
-	{
-		// Take care of all GLFW events
-		processInput(window);
-
-		// Specify the color of the background
-		glClearColor(0.07f, 0.13f, 0.17f, 1.0f);
-		// Clean the back buffer and assign the new color to it
-		glClear(GL_COLOR_BUFFER_BIT);
-
-		glfwSwapBuffers(window);
-		glfwPollEvents();
-	}
-
-	// Delete window before ending the program
-	glfwDestroyWindow(window);
-	// Terminate GLFW before ending the program
-	glfwTerminate();
 	return 0;
 }
