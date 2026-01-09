3240b28092188ccbf2fcd06f04f778542a3e787b
AlfredIsAPicoByte <19atang-agama.a@nef.tynecoast.academy>
2026-01-09 18:10:26 +0000

    Fixed some object tranformations in scens

--- py_src/src/Raytracing.py
+++ py_src/src/Raytracing.py
@@ -851,7 +851,7 @@ class NormalShading(ShadingStrategy):
         normal = getattr(hit_info, "normal", np.array([0.0, 1.0, 0.0]))
         
         # 2. Map from range [-1, 1] to [0, 1] for color display
-        # Normal (0,0,0) becomes Gray (0.5, 0.5, 0.5)
+        # Normal (0,0,0) becomes Black (0, 0, 0)
         r = np.clip((normal[0]), 0.0, 1.0)
         g = np.clip((normal[1]), 0.0, 1.0)
         b = np.clip((normal[2]), 0.0, 1.0)
